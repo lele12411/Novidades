ff1d08fa49d2d85b716ede465d179ae2bc6691ab
lele12411 <lestevesdesouza@gmail.com>
2025-04-27 16:27:45 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Novidades</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1 class="titulo">Novidades</h1>
+    <div class="container">
+        <h2 class="texto">Inteligência Artificial na Indústria de Anime e Mangá</h2>
+        <p class="texto">
+   
