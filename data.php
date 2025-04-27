dd84f40ff9fcde01f8be61d14b71b2c3d7f7ceeb
lele12411 <lestevesdesouza@gmail.com>
2025-04-27 16:30:14 -0300

    data.php

---
+++ data.php
@@ -0,0 +1,13 @@
+<?php
+$texto = "A influência da inteligência artificial (IA) na indústria de anime e mangá está se expandindo rapidamente,
+com várias iniciativas sendo implementadas para explorar suas capacidades.
+O estúdio OLM Digital, conhecido por séries como Pokémon e Berserk,
+planeja incorporar IA generativa na produção de animes.
+O representante do estúdio, Tatsuo Yotsukura, 
+mencionou que a IA pode se tornar um novo padrão na indústria,
+assim como o 3D se tornou no passado.
+Ele está supervisionando um projeto chamado ANIMINS,
+que visa estudar a implementação da IA na produção de animes.";
+
+echo $texto;
+?>
